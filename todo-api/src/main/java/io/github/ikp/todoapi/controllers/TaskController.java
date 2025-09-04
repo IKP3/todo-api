da54672846197dfaccdd650c10090f5889c23ddb
IKP3 <igor.przybytek3@gmail.com>
2025-09-04 15:50:58 +0100

    use TaskService in TaskController createTask instead of calling repositories

--- todo-api/src/main/java/io/github/ikp/todoapi/controllers/TaskController.java
+++ todo-api/src/main/java/io/github/ikp/todoapi/controllers/TaskController.java
@@ -5,8 +5,10 @@ import io.github.ikp.todoapi.domain.entities.TaskEntity;
 import io.github.ikp.todoapi.mappers.Mapper;
 import io.github.ikp.todoapi.repositories.UserRepository;
 import io.github.ikp.todoapi.services.TaskService;
+import io.github.ikp.todoapi.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,18 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TaskController {
   private final TaskService taskService;
+  private final UserService userService;
   private final Mapper<TaskEntity, TaskDto> taskMapper;
-  private final UserRepository userRepository;
 
-  public TaskController(final TaskService taskService,final Mapper<TaskEntity, TaskDto> taskMapper,
-      UserRepository userRepository) {
+  public TaskController(final TaskService taskService,final UserService userService,final Mapper<TaskEntity, TaskDto> taskMapper) {
     this.taskService = taskService;
     this.taskMapper = taskMapper;
-    this.userRepository = userRepository;
+    this.userService = userService;
   }
   @PostMapping(path = "/users/{userId}/tasks")
   public ResponseEntity<TaskDto> createTask( @PathVariable Long userId,@RequestBody TaskDto taskDto){
-    return userRepository.findById(userId)
+    return userService.getUser(userId)
         .map(user -> {
           TaskEntity task = taskMapper.mapFrom(taskDto);
           task.setUser(user);
@@ -35,4 +36,8 @@ public class TaskController {
         })
         .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
+  /*@GetMapping(path = "/users/{userId}/tasks/{taskId}")
+  public ResponseEntity<TaskDto> getTask(@PathVariable Long userId,@PathVariable Long taskId){
+
+  }*/
 }
